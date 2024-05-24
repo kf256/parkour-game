be32c3e87ee4581728e99d8a77e9743b3d2c3f39
Kalle <kf101@mail.de>
2024-05-24 17:19:24 +0200

    update canvas automatically

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
+updateCanvasSize();
+
 update();
 
 function update() {
     ctx.fillStyle = `HSL(${Date.now()/100%360}, 100%, 50%)`;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    requestAnimationFrame(update);
 }
 
-addEventListener("resize", function() {
+addEventListener("resize", updateCanvasSize);
+
+function updateCanvasSize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-});
+}

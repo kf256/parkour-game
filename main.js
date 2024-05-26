ed210a10606ec5f469c5ad3ec85fa20cbfc3f939
Kalle <kf101@mail.de>
2024-05-26 13:42:14 +0200

    fixed an error

--- main.js
+++ main.js
@@ -92,6 +92,56 @@ class Obstacle {
     }
 }
 
+// information about which arrow keys are pressed
+let arrows = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+};
+
+// event listeners updating the arrows object
+addEventListener("keydown", (evt) => {
+    switch (evt.key) {
+        case "ArrowLeft": {
+            arrows.left = true;
+            break;
+        }
+        case "ArrowRight": {
+            arrows.right = true;
+            break;
+        }
+        case "ArrowUp": {
+            arrows.up = true;
+            break;
+        }
+        case "ArrowDown": {
+            arrows.down = true;
+            break;
+        }
+    }
+});
+addEventListener("keyup", (evt) => {
+    switch (evt.key) {
+        case "ArrowLeft": {
+            arrows.left = false;
+            break;
+        }
+        case "ArrowRight": {
+            arrows.right = false;
+            break;
+        }
+        case "ArrowUp": {
+            arrows.up = false;
+            break;
+        }
+        case "ArrowDown": {
+            arrows.down = false;
+            break;
+        }
+    }
+});
+
 main();
 
 function main() {
@@ -165,53 +215,3 @@ function updateCanvasSize() {
     // scale the contents of the canvas to make drawing easier
     ctx.scale(50, 50);
 }
-
-// information about which arrow keys are pressed
-let arrows = {
-    up: false,
-    down: false,
-    left: false,
-    right: false,
-};
-
-// event listeners updating the arrows object
-addEventListener("keydown", (evt) => {
-    switch (evt.key) {
-        case "ArrowLeft": {
-            arrows.left = true;
-            break;
-        }
-        case "ArrowRight": {
-            arrows.right = true;
-            break;
-        }
-        case "ArrowUp": {
-            arrows.up = true;
-            break;
-        }
-        case "ArrowDown": {
-            arrows.down = true;
-            break;
-        }
-    }
-});
-addEventListener("keyup", (evt) => {
-    switch (evt.key) {
-        case "ArrowLeft": {
-            arrows.left = false;
-            break;
-        }
-        case "ArrowRight": {
-            arrows.right = false;
-            break;
-        }
-        case "ArrowUp": {
-            arrows.up = false;
-            break;
-        }
-        case "ArrowDown": {
-            arrows.down = false;
-            break;
-        }
-    }
-});

18eff45ced8af23e84c61a23e22ccce6f9884cf7
Kalle <kf101@mail.de>
2024-05-29 17:31:01 +0200

    modify the obstacles

--- main.js
+++ main.js
@@ -62,8 +62,8 @@ function main() {
     new Obstacle(12,  0,  1, 14);
     new Obstacle( 4,  9,  9,  1);
     new Obstacle( 1, 11,  1,  2);
-    new Obstacle(10,  8,  1,  1);
-    new Obstacle(11,  6,  1,  3);
+    new Obstacle(10,  7,  1,  2);
+    new Obstacle(11,  5,  1,  4);
     new Obstacle( 1, 13, 11,  1);
     
     // calculate target.borders

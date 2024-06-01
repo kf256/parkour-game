a2a671c8e245284517591853e977bc69a3e1fd4e
Kalle <kf101@mail.de>
2024-06-01 12:08:43 +0200

    display messages in messages div

--- main.js
+++ main.js
@@ -170,10 +170,18 @@ function update() {
     
     // check if the user won
     let maxTargetDist = Math.max(...Object.values(target.dist));
-    if (maxTargetDist <= 0) gameStatus = "won";
+    if (maxTargetDist <= 0) {
+        gameStatus = "won";
+        document.getElementById("messages").style.backgroundColor = "#8888";
+        document.getElementById("messages").innerHTML = "You won! Well done!";
+    }
     
     // check if the user lost
-    if (player.posY > maxY) gameStatus = "lost";
+    if (player.posY > maxY) {
+        gameStatus = "lost";
+        document.getElementById("messages").style.backgroundColor = "#8888";
+        document.getElementById("messages").innerHTML = "Oh, no! You lost. Never mind! Try again!";
+    }
     
     // call update() again as soon as possible
     if (gameStatus === "running") setTimeout(update, 0);
@@ -192,24 +200,6 @@ function draw() {
     // draw the player character
     ctx.fillStyle = "yellow";
     ctx.fillRect(player.posX, player.posY, player.width, player.height);
-    
-    if (gameStatus === "won") {
-        ctx.fillStyle = "#ccc8";
-        ctx.fillRect(0, 0, canvas.width/50, canvas.height/50);
-        ctx.font = "1px sans-serif";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillStyle = "black";
-        ctx.fillText("You won! Well done!", canvas.width/50/2, canvas.height/50/2);
-    } else if (gameStatus === "lost") {
-        ctx.fillStyle = "#4448";
-        ctx.fillRect(0, 0, canvas.width/50, canvas.height/50);
-        ctx.font = "1px sans-serif";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillStyle = "black";
-        ctx.fillText("Oh, no! You lost. Never mind! Try again!", canvas.width/50/2, canvas.height/50/2);
-    }
     
     // call draw() again for the next frame
     if (gameStatus === "running") requestAnimationFrame(draw);

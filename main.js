6f6aa71844dc5d255024bd67da579799969168a7
Kalle <kf101@mail.de>
2024-06-02 17:41:44 +0200

    add a level

--- main.js
+++ main.js
@@ -43,6 +43,41 @@ let levels = [
         new Obstacle(12,  5,  1,  4);
         new Obstacle( 1, 13, 12,  1);
     },
+    function() {
+        // position the player
+        player = {
+            posX: 11,
+            posY: 11.1,
+            velX: 0,
+            velY: 0,
+            width: 0.9,
+            height: 1.9,
+            climb: false,
+        };
+        
+        // position the target
+        target = {
+            posX: 1,
+            posY: 1,
+            width: 1,
+            height: 1,
+        };
+        
+        // add some obstacles
+        new Obstacle( 1,  3,  2,  1);
+        new Obstacle( 4,  2,  1,  2);
+        new Obstacle( 2,  4,  3,  1);
+        new Obstacle( 4,  5,  4,  1);
+        new Obstacle( 7,  4,  1,  2);
+        new Obstacle( 8,  3,  1,  2);
+        new Obstacle( 0,  0,  1, 14);
+        new Obstacle(13,  0,  1, 14);
+        new Obstacle( 5,  9,  8,  1);
+        new Obstacle( 1, 11,  1,  2);
+        new Obstacle(11,  7,  1,  2);
+        new Obstacle(12,  5,  1,  4);
+        new Obstacle( 1, 13, 12,  1);
+    },
 ];
 
 let gameStatus = "menu";

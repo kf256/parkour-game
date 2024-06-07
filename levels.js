1075b49bb24fbd7b96a8e6bee55b785619802870
Kalle <kf101@mail.de>
2024-06-07 16:04:31 +0200

    add a level

--- levels.js
+++ levels.js
@@ -131,6 +131,45 @@ export const levels = [
     function() {
         // position the player
         player = {
+            posX: 0,
+            posY: 11.1,
+            velX: 0,
+            velY: 0,
+            width: 0.9,
+            height: 1.9,
+            climb: false,
+        };
+        
+        // position the target
+        target = {
+            posX: 1.1,
+            posY: 0.1,
+            width: 0.8,
+            height: 0.8,
+        };
+        
+        // add some obstacles
+        new Obstacle( 0, 13,  1,  1);
+        new Obstacle( 4, 11,  1,  1);
+        new Obstacle( 9, 12,  1,  1);
+        new Obstacle(12, 10,  1,  1);
+        new Obstacle(17,  9,  1,  1);
+        new Obstacle(20, 11,  1,  1);
+        new Obstacle(22,  9,  1,  1);
+        new Obstacle(23,  7,  1,  1);
+        new Obstacle(24,  5,  1,  1);
+        new Obstacle(21,  3,  1,  1);
+        new Obstacle(17,  3,  1,  1);
+        new Obstacle(13,  3,  1,  1);
+        new Obstacle( 9,  4,  1,  1);
+        new Obstacle( 6,  4,  1,  1);
+        new Obstacle( 3,  3,  1,  1);
+        new Obstacle( 1,  1,  1,  1);
+        new Obstacle( 0,  0,  1,  1);
+    },
+    function() {
+        // position the player
+        player = {
             posX: 4,
             posY: 5.1,
             velX: 0,
